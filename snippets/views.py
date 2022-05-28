220c7f3a4cb0bc3c553b50e3409bdc2f5bfb358f
ryumada <rizukiryumada@gmail.com>
2022-05-28 21:25:01 +0700

    Using Router
    ... for automatically register the url, because we have used ViewSets
    ~ api_root is automatically created while we using router

--- snippets/views.py
+++ snippets/views.py
@@ -1,19 +1,12 @@
 from django.contrib.auth.models import User
 from rest_framework import permissions, renderers, viewsets
-from rest_framework.decorators import action, api_view
+from rest_framework.decorators import action
 from rest_framework.response import Response
-from rest_framework.reverse import reverse
+
 from snippets.models import Snippet
 from snippets.permissions import IsOwnerOrReadOnly
 from snippets.serializers import SnippetSerializer, UserSerializer
 
-@api_view(['GET'])
-def api_root(request, format=None):
-  return Response({
-    'users': reverse('user-list', request=request, format=format),
-    'snippets': reverse('snippet-list', request=request, format=format),
-  })
-  
 class SnippetViewSet(viewsets.ModelViewSet):
   """
   This viewset automatically provides `list`, `create`, `retrieve`, `update`, and `destroy` actions.
